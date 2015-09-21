cbb6672686ff237bc5378b6a990e5d4e0bcc3e59
antonwass <anton_wass@hotmail.com>
2015-09-21 11:37:57 +0200

    first

--- uppgift_a/src/uppgift_a/Line.java
+++ uppgift_a/src/uppgift_a/Line.java
@@ -17,8 +17,9 @@ public class Line extends Shape{
     private double x2, y2;
     
     public Line(double x, double y, Color color, double x2, double y2){
-        super();
-        
+        super(x, y, color);
+        this.x2 = x2;
+        this.y2 = y2;
     }
     
     public double getX2(){
@@ -29,8 +30,35 @@ public class Line extends Shape{
         return y2;
     }
     
+    @Override
+    public void move(long elapsedTimeNs){
+        setX(getX() + getDx() * elapsedTimeNs / BILLION);
+        setY(getY() + getDy() * elapsedTimeNs / BILLION);
+        x2 += getDx() * elapsedTimeNs / BILLION;
+        y2 += getDy()* elapsedTimeNs / BILLION;
+    }
     
     @Override
+    public void constrain (double boxX, double boxY, 
+            double boxWidth, double boxHeight){
+        
+        if (getX() < boxX || x2 < boxX) {
+            setVelocity(Math.abs(getDx()), getDy());
+        } else if (getX() > boxWidth || x2 > boxWidth) {
+            setVelocity(-Math.abs(getDx()), getDy());
+        }
+        if (getY() < boxY || y2 < boxY) {
+            setVelocity(getDx(), Math.abs(getDy()));
+        } else if (getY() > boxHeight || y2 > boxHeight) {
+            setVelocity(getDx(), -Math.abs(getDy()));
+        }
+
+        
+    }
+    @Override
     public void paint(GraphicsContext gc){
+        gc.setStroke(getColor());
+        gc.strokeLine(super.getX(), super.getY(), x2, y2);
+        
     }
 }
